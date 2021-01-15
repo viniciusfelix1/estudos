189341cddd5eb78793ceb3df176dcc0b66f32f49
Vinicius Felix <vinicius.felix@4linux.com.br>
2021-01-15 15:55:27 -0300

    Adicionado variaves sobre o ambiente

--- terraform/rds/vars.tf
+++ terraform/rds/vars.tf
@@ -3,17 +3,32 @@ variable "AWS_REGION" {
 }
 
 variable "rds_engine" {
-  description = "O nome que será dado ao cluster."
+  description = "O SGBD que será utilizado."
   default = "mariadb"
 }
 
 variable "rds_engine_version" {
-  description = "O nome que será dado ao cluster."
+  description = "A versão do SGBD."
   default = "10.4.8"
 }
 
-
 variable "rds_instance_class" {
-  description = "O nome que será dado ao cluster."
+  description = "A classe de recursos utilizada."
   default = "db.m5.large"
 }
+
+variable "rds_name" {
+  description = "O nome que será dado à instancia."
+  default = "vinicius"
+}
+
+variable "rds_username" {
+  description = "O usuário de acesso à instância."
+  default = "vinicius"
+}
+
+variable "rds_password" {
+  description = "A senha do usuário."
+  default = "vinicius123"
+}
+
